c08604b2c51544f3e142fcfad5c8993264ee9d81
Rafael Marques <rafael.marques.rodrigues95@gmail.com>
2015-05-18 16:05:14 -0300

    added main

---
+++ src/br/usp/icmc/poo/TurmaA015/LibraryOrganizer/LibraryOrganizer.java
@@ -0,0 +1,15 @@
+
+package br.usp.icmc.poo.TurmaA015.LibraryOrganizer;
+
+import br.usp.icmc.poo.TurmaA015.Rentable.*;
+import br.usp.icmc.poo.TurmaA015.Person.*;
+import br.usp.icmc.poo.TurmaA015.Library.*;
+
+import java.io.*;
+import java.util.*;
+
+public class LibraryOrganizer {
+	public static void main(String[] args){
+		System.out.println("System starting...");
+	}
+}
