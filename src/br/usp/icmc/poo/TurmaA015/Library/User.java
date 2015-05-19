1b819fe093d74feb5958af8a0c2e79ef6f9d9cdd
Rafael Marques <rafael.marques.rodrigues95@gmail.com>
2015-05-19 10:02:53 -0300

    user changes

--- src/br/usp/icmc/poo/TurmaA015/Library/User.java
+++ src/br/usp/icmc/poo/TurmaA015/Library/User.java
@@ -6,13 +6,40 @@ import br.usp.icmc.poo.TurmaA015.Person.*;
 import java.util.Map;
 import java.util.HashMap;
 
+//classe para facilitar o trabalho da biblioteca para guardar os dados
+//cada pessoa que aluga um livro na biblioteca tem seu respectivo usuario na biblioteca
+//o usuário deve guardar:
+//1 - o atraso de cada pessoa em relação à um respectivo livro (hashmap)
+//2 - os livros em posse de cada pessoa (arraylist)
+//3 - o tempo que a pessoa está com o livro
 public class User {
 	private Person person;
 	private Map<Rentable, Integer> delays;
+	private Map<Rentable, Integer> rentTime;
+	private Arraylist<Rentable> ownedFiles;
 
 	public User(Person p){
 		person = p;
 		delays = new HashMap<Rentable, Integer>();
+		rentTime = new HashMap<Rentable, Integer>();
+		ownedFiles = new Arraylist<Rentable>();
+	}
+
+	public void addFile(Rentable r){
+		ownedFiles.add(r);
+	}
+
+	public void refundFile(Rentable r){
+		ownedFiles.remove(r);
+	}
+
+	public boolean increaseRentTime(Rentable r, int time){
+		if(rentTime.containsKey(r)){
+			rentTime.put(r, new Integer((rentTime.get(r)).intValue() + time));
+			return true;
+		}
+
+		return false;
 	}
 
 	public Person getPerson(){
